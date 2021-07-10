882920325bca82911c759c928bcd72b3f7e52cb2
Jaroslaw Nazaruk <jaroslaw.nazaruk@gmail.com>
2021-07-10 22:58:06 +0200

    add services

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const singUpTemplateCopy = require('../models/SingUpModels')
-const validate = require('../middlewares/validate');
 
 router.post('/signup', (request, response) => {
    const singedUpUser = new singUpTemplateCopy({
